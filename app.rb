fd22fd9bd99700c993c6b2ede6990e2228f8ccaf
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-13 16:41:27 +0630

    create list_people method in app.rb

--- app.rb
+++ app.rb
@@ -20,4 +20,8 @@ class App
         @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
     end
 
+
+    def list_people
+        @people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+    end
 end

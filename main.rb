cd0800941b0c54c13b88965491efbef060c4f307
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-13 20:17:06 +0630

    refractor the main.rb to fix rubocop errors

--- main.rb
+++ main.rb
@@ -1,33 +1,46 @@
 require_relative 'app'
 
+def list_options
+  'Please choose an option by entering a number:
+    1 - List of all books
+    2 - List of all people
+    3 - Add a person
+    4 - Create a book
+    5 - Add a rental
+    6 - List of all rental for given id
+    7 - Exit'
+end
+
+def option(option, app) # rubocop:disable Metrics/CyclomaticComplexity
+  case option
+  when 1
+    app.list_books
+  when 2
+    app.list_people
+  when 3
+    app.create_person
+  when 4
+    app.create_book
+  when 5
+    app.create_rental
+  when 6
+    app.list_rentals
+  when 7
+    exit
+  else
+    puts 'Invalid option, please type correct number!'
+  end
+end
+
 def main
   app = App.new
 
   puts "Welcome to School Library App!\n\n"
 
   loop do
-    puts 'Please choose an option by entering a number: '
-    puts '1 - List of all books'
-    puts '2 - List of all people'
-    puts '3 - Add a person'
-    puts '4 - Create a book'
-    puts '5 - Add a rental'
-    puts '6 - List of all rental for given id'
-    puts '7 - Exit'
+    puts list_options
     option = gets.chomp.to_i
-
-    case option
-    when 1 then app.list_books
-    when 2 then app.list_people
-    when 3 then app.create_person
-    when 4 then app.create_book
-    when 5 then app.create_rental
-    when 6 then app.list_rentals
-    when 7
-      break
-    else
-      puts 'Invalid option, please type correct number!'
-    end
+    puts option(option, app)
   end
 end
 

1b5e912db6d25a1beb563dbbabdb1351ff7bd133
James Serengia <jamesserengia@gmail.com>
2023-04-17 16:07:20 +0300

    Updated main.rb file

--- main.rb
+++ main.rb
@@ -2,20 +2,24 @@ require_relative 'app'
 require_relative 'option'
 
 def list_options
-  'Please choose an option by entering a number:
+  "Please choose an option by entering a number:
     1 - List of all books
     2 - List of all people
     3 - Add a person
     4 - Create a book
     5 - Add a rental
     6 - List of all rental for given id
-    7 - Exit'
+    7 - Exit"
+end
+
+def intro
+  "Welcome to School Library App!\n\n"
 end
 
 def main
   app = App.new
 
-  puts "Welcome to School Library App!\n\n"
+  puts intro
 
   loop do
     puts list_options

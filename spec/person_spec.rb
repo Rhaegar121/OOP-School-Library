7c10506ae0a4664c593ca14b40dfdf16dd0ebe37
James Serengia <jamesserengia@gmail.com>
2023-04-20 11:52:13 +0300

    Add person class tests

---
+++ spec/person_spec.rb
@@ -0,0 +1,25 @@
+require_relative "spec_helper"
+
+describe "Person" do
+  before(:each) do
+    @person = Person.new(12, "Peter", parent_permission)
+  end
+
+  describe "name" do
+    it "returns the correct name" do
+      expect(@person.name).to eql "Peter"
+    end
+  end
+
+  describe "age" do
+    it "returns the correct age" do
+      expect(@person.author).to eql 12
+    end
+  end
+
+  describe "parent_permission" do
+    it "returns the correct parent_permission" do
+      expect(@person.parent_permission).to be false
+    end
+  end
+end

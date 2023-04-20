8863ce039b324d2057079e46f6b0688d96883c0b
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-20 14:01:34 +0630

    add test for Book class in book_dpec.rb

---
+++ spec/book_spec.rb
@@ -0,0 +1,19 @@
+require_relative 'spec_helper'
+
+describe 'Book' do
+    before(:each) do
+        @book = Book.new('Title', 'Author')
+    end
+
+    describe 'title' do
+    it 'returns the correct title' do
+      expect(@book.title).to eql 'Title'
+    end
+  end
+
+  describe 'author' do
+    it 'returns the correct author' do
+      expect(@book.author).to eql 'Author'
+    end
+  end
+end
